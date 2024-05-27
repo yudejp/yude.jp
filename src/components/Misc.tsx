c27109224a9c4cd13ad113339d71c1b8b12ad4cf
yude <i@yude.jp>
2024-05-27 22:47:23 +0900

    add buttons

--- src/components/Misc.tsx
+++ src/components/Misc.tsx
@@ -9,14 +9,31 @@ export default function Weather() {
                         height={196}
                     />
                 </div>
-                <div className="inline-block ml-3">
+                <div
+                    className="inline-block ml-3"
+                    style={{
+                        imageRendering: `pixelated`
+                    }}
+                >
                     <img width={94} src="buttons/009button.gif" />
                     <img width={94} src="buttons/mac.gif" />
                     <img width={94} src="buttons/right2repair.png" />
                     <img width={94} src="buttons/vscbutton.gif" />
                     <img width={94} src="buttons/parental.png" />
                     <img width={94} src="buttons/www.gif" />
-
+                </div>
+                <div
+                    className="inline-block ml-3"
+                    style={{
+                        imageRendering: `pixelated`
+                    }}
+                >
+                    <img width={94} src="buttons/discord.gif" />
+                    <img width={94} src="buttons/hackerpowered.gif" />
+                    <img width={94} src="buttons/linux_powered.gif" />
+                    <img width={94} src="buttons/written-in-vi.gif" />
+                    <img width={94} src="buttons/desp-anim.gif" />
+                    <img width={94} src="buttons/RaveNow3.gif" />
                 </div>
             </div>
         </div>
